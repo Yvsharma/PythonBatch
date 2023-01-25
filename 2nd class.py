36fc9b186eb6ccc65abeb747c367e7629be8cf29
Yashvardhan sharma <95120981+Yvsharma@users.noreply.github.com>
2023-01-25 17:49:57 +0530

    2d day of python class
    discussing about data types and performing some basic operations

---
+++ 2nd class.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=5
+
+
+# In[2]:
+
+
+a
+
+
+# In[3]:
+
+
+print(a)
+# print is used to see output
+
+
+# In[4]:
+
+
+a=6
+a
+
+
+# In[5]:
+
+
+type(a)
+
+
+# In[6]:
+
+
+a=2.5
+type(a)
+
+
+# In[7]:
+
+
+a='string'
+
+
+# In[8]:
+
+
+type(a)
+
+
+# In[9]:
+
+
+a="string"
+type(a)
+
+
+# In[10]:
+
+
+a=2+5
+a
+
+
+# In[11]:
+
+
+a='python'
+a[1]
+
+
+# In[12]:
+
+
+a[-5]
+
+
+# In[13]:
+
+
+y=True
+z=False
+y&z
+
+
+# In[14]:
+
+
+and = &
+or+ |
+
+
+# In[15]:
+
+
+a&A
+
+
+# In[1]:
+
+
+a&A
+
+
+# In[2]:
+
+
+A&B
+
+
+# In[6]:
+
+
+a=5
+b=6
+
+
+# In[2]:
+
+
+a>b
+
+
+# In[3]:
+
+
+a<b
+
+
+# In[4]:
+
+
+a=b
+
+
+# In[5]:
+
+
+a==b
+
+
+# In[7]:
+
+
+a==b
+
+
+# In[8]:
+
+
+Name= 'Yashvardhan'
+
+
+# In[9]:
+
+
+surname='Sharma'
+
+
+# In[11]:
+
+
+print(Name+surname)
+
+
+# In[12]:
+
+
+print(Name+' '+surname)
+
+
+# In[13]:
+
+
+line= '''we are writing
+multiline
+xomment'''
+
+
+# In[14]:
+
+
+line
+
+
+# # first class
+
+# ### datatype
+
+# In[15]:
+
+
+1st="winner"
+
+
+# In[ ]:
+
+
+
+
+
+# In[16]:
+
+
+A=5
+a=10
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
